b6154c6490559346e9d97491c9b06367e275fbd5
PUSHkin42342443 <alexandderdudarev@gmail.com>
2021-12-09 16:51:32 +0500

    выполнено задание 7

---
+++ task7/task7/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double resultOne;
+            Console.WriteLine("Введите первый х для ф от тангенса х:");
+            double firstXEnter = double.Parse(Console.ReadLine());
+            double a = -(Math.PI) / 2;
+            double b = (Math.PI) / 2;
+            if (firstXEnter < a)
+            {
+                if (firstXEnter > b)
+                {
+                    resultOne = FirstX(firstXEnter);
+                    Console.WriteLine($"функция f от х = {resultOne:F3}");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("ты  попутал значения");
+
+
+            }
+
+            Console.WriteLine("Введите второй х для ф от тангенса х:");
+            double secondXEnter = double.Parse(Console.ReadLine());
+            double c = (Math.PI) / 2;
+            double resultTwo;
+            if ( secondXEnter >= c )
+            {
+                resultTwo = DoubleX(secondXEnter);
+                Console.WriteLine(resultTwo);
+
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("снова попутал, ахахахахах");
+            }
+
+            Console.WriteLine("Введите третий х для ф от тангенса х:");
+            double thirdXEnter = double.Parse(Console.ReadLine());
+            double d = (Math.PI) / 2;
+            double resultTree;
+            if (thirdXEnter >= d)
+            {
+                resultTree = ThirdX(thirdXEnter);
+                Console.WriteLine(resultTree);
+
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("снова попутал, ну чел... у же не смешно");
+
+
+                Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+                Console.WriteLine("⠄⠄⠄⠄⣠⠞⠉⢉⠩⢍⡙⠛⠋⣉⠉⠍⢉⣉⣉⣉⠩⢉⠉⠛⠲⣄⠄⠄⠄⠄");
+                Console.WriteLine("⠄⠄⠄⡴⠁⠄⠂⡠⠑⠄⠄⠄⠂⠄⠄⠄⠄⠠⠄⠄⠐⠁⢊⠄⠄⠈⢦⠄⠄⠄");
+                Console.WriteLine("⠄⣠⡾⠁⠄⠄⠄⣴⡪⠽⣿⡓⢦⠄⠄⡀⠄⣠⢖⣻⣿⣒⣦⠄⡀⢀⣈⢦⡀⠄");
+                Console.WriteLine("⣰⠑⢰⠋⢩⡙⠒⠦⠖⠋⠄⠈⠁⠄⠄⠄⠄⠈⠉⠄⠘⠦⠤⠴⠒⡟⠲⡌⠛⣆");
+                Console.WriteLine("⢹⡰⡸⠈⢻⣈⠓⡦⢤⣀⡀⢾⠩⠤⠄⠄⠤⠌⡳⠐⣒⣠⣤⠖⢋⡟⠒⡏⡄⡟");
+                Console.WriteLine("⠄⠙⢆⠄⠄⠻⡙⡿⢦⣄⣹⠙⠒⢲⠦⠴⡖⠒⠚⣏⣁⣤⣾⢚⡝⠁⠄⣨⠞⠄");
+                Console.WriteLine("⠄⠄⠈⢧⠄⠄⠙⢧⡀⠈⡟⠛⠷⡾⣶⣾⣷⠾⠛⢻⠉⢀⡽⠋⠄⠄⣰⠃⠄⠄");
+                Console.WriteLine("⠄⠄⠄⠄⠑⢤⡠⢂⠌⡛⠦⠤⣄⣇⣀⣀⣸⣀⡤⠼⠚⡉⢄⠠⣠⠞⠁⠄⠄⠄");
+                Console.WriteLine("⠄⠄⠄⠄⠄⠄⠉⠓⠮⣔⡁⠦⠄⣤⠤⠤⣤⠄⠰⠌⣂⡬⠖⠋⠄⠄⠄⠄⠄⠄");
+                Console.WriteLine("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠒⠤⢤⣀⣀⡤⠴⠒⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
+
+
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+        static double FirstX (double x)
+        {
+            return Math.Tan(x);         
+        }
+        static double DoubleX(double y)
+        {
+            return 1 / y;
+        }
+        static double ThirdX(double z)
+        {
+
+            return -(1 / z);
+        }
+    }
+}
